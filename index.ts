bb788a8e2a3294135a163e25c6f85f9d6edc035f
taye000 <taylorgitari@gmail.com>
2023-01-13 11:02:54 +0300

    index

--- index.ts
+++ index.ts
@@ -1,10 +1,11 @@
-import { Express } from "express";
+import express from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
 
 const app = express();
 
-app.use(bodyParser.json({ limit: "30mb" }));
-app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
+
