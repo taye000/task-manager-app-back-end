bb809619bb5a94a89e69ce13a621b98845afb378
taye000 <taylorgitari@gmail.com>
2023-01-17 11:32:53 +0300

    task controller

--- controllers/tasks.ts
+++ controllers/tasks.ts
@@ -4,7 +4,7 @@ import CreateTask from "../models/createTask";
 export const getTasks = async (req: any, res: any) => {
   try {
     const allTasks = await CreateTask.find();
-    res.status(200).json({"tasks":["task1"]});
+    res.status(200).json({});
   } catch (error: any) {
     res.status(404).json({ error: error.message });
   }
