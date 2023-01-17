c4094ab72c581ace8e7d6b648529ddb21548fb1c
taye000 <taylorgitari@gmail.com>
2023-01-17 17:16:52 +0300

    crud

--- controllers/tasks.ts
+++ controllers/tasks.ts
@@ -4,20 +4,20 @@ import CreateTask from "../models/createTask";
 export const getTasks = async (req: any, res: any) => {
   try {
     const allTasks = await CreateTask.find();
-    res.status(200).json({allTasks});
+    res.status(200).json({ allTasks });
   } catch (error: any) {
     res.status(404).json({ error: error.message });
   }
 };
 
 //fetch task by id
-export const getTaskbyId = async (id: string) => {
+export const getTaskbyId = async (req: any, res: any) => {
   try {
+    const { id } = req.params;
     const taskById = await CreateTask.findById(id);
-    return JSON.stringify(taskById);
+    res.status(200).json({ taskById });
   } catch (error: any) {
-    console.log(error.message);
-    return null;
+    res.status(404).json({ error: error.message });
   }
 };
 
@@ -36,5 +36,33 @@ export const createTask = async (req: any, res: any) => {
     console.log(createdTask);
   } catch (error: any) {
     res.status(409).json({ error: error.message });
+  }
+};
+
+//update task by id
+export const updateTask = async (req: any, res: any) => {
+  try {
+    const { id } = req.params;
+    const { task, day, reminder } = req.body;
+    const taskById: any = await CreateTask.findByIdAndUpdate(id);
+    if (task) taskById.task = task;
+    if (day) taskById.day = day;
+    if (reminder) taskById.reminder = reminder;
+    taskById.save();
+    res.status(200).json({ taskById });
+  } catch (error: any) {
+    res.status(404).json({ error: error.message });
+  }
+};
+
+//delete task by id
+export const deleteTask = async (req: any, res: any) => {
+  try {
+    const { id } = req.params;
+    await CreateTask.findByIdAndDelete(id);
+    const allTasks = await CreateTask.find();
+    res.status(200).json({ allTasks });
+  } catch (error: any) {
+    res.status(404).json({ error: error.message });
   }
 };
--- routes/tasks.ts
+++ routes/tasks.ts
@@ -1,10 +1,12 @@
 import express from "express";
-import { getTasks, getTaskbyId, createTask } from "../controllers/tasks";
+import { getTasks, getTaskbyId, createTask, updateTask, deleteTask } from "../controllers/tasks";
 
 const router = express.Router();
 
 router.get("/", getTasks);
 router.get("/:id", getTaskbyId);
 router.post("/create", createTask);
+router.patch("/:id", updateTask);
+router.delete("/:id", deleteTask);
 
 export default router;
